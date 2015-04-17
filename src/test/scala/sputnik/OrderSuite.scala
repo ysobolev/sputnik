c632bddd2ef222a88f02c0d2c4f45bc085d45d8c
Sameer Parekh Brenn <sameer@m2.io>
2015-04-17 10:57:45 -0600

    flesh out tests

--- src/test/scala/sputnik/OrderSuite.scala
+++ src/test/scala/sputnik/OrderSuite.scala
@@ -1,25 +1,8 @@
 package sputnik
 
 import org.scalatest.FunSuite
-import com.github.nscala_time.time.Imports._
 
-/**
- * Created by sameer on 4/17/15.
- */
-
-class OrderSuite extends FunSuite {
-  val buy100At100 = Order(1, 100, 100, DateTime.now, "BUY", "test")
-  val buy100At50 = Order(1, 100, 50, DateTime.now, "BUY", "test")
-
-  val sell100At50 = Order(2, 100, 50, DateTime.now, "SELL", "test")
-  val sell100At150 = Order(3, 100, 150, DateTime.now, "SELL", "test")
-
-  val now = DateTime.now
-  val sell100At100Now = Order(4, 100, 100, now, "SELL", "test")
-  val sell100At100In5Min = Order(5, 100, 100, now + 5.minutes, "SELL", "test")
-
-  val buy100At100Now = Order(4, 100, 100, now, "BUY", "test")
-  val buy100At100In5Min = Order(5, 100, 100, now + 5.minutes, "BUY", "test")
+class OrderSuite extends FunSuite with Orders {
 
   test("matches correctly") {
     assert(buy100At100 matches sell100At50, "Buy 100@100 matches Sell 100@50")
