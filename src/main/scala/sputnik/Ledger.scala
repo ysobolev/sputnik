fd4dc4c2db84198f306bb9033f865c6223711cc2
Sameer Parekh Brenn <sameer@m2.io>
2015-04-24 16:37:16 -0600

    "complete" the ledger

--- src/main/scala/sputnik/Ledger.scala
+++ src/main/scala/sputnik/Ledger.scala
@@ -1,6 +1,6 @@
 package sputnik
 
-import akka.actor.{Actor, ActorRef}
+import akka.actor.{Props, Actor, ActorRef}
 import com.github.nscala_time.time.Imports._
 import sputnik.LedgerDirection._
 import sputnik.LedgerSide._
@@ -12,9 +12,11 @@ class LedgerException(x: String) extends Exception(x)
 
 abstract class LedgerMessage
 
-case class NewPosting(posting: Posting, count: Int, uuid: String) extends LedgerMessage
-
+case class NewPosting(count: Int, posting: Posting, uuid: String) extends LedgerMessage
+case class AddPosting(count: Int, posting: Posting, sender: ActorRef) extends LedgerMessage
+case class PostingResult(result: Boolean) extends LedgerMessage
 case class GetBalance(user: User) extends LedgerMessage
+case class NewJournal(journal: Journal) extends LedgerMessage
 
 case class Posting(contract: Contract, user: User, quantity: Quantity, direction: LedgerDirection) {
   val timestamp = DateTime.now
@@ -33,9 +35,14 @@ case class Posting(contract: Contract, user: User, quantity: Quantity, direction
   lazy val signedQuantity = sign * quantity
 }
 
-class PostingGroup(uid: String, count: Int, postings: List[Posting], senders: List[ActorRef]) {
-  def add(posting: Posting, sender: ActorRef): PostingGroup = {
-    new PostingGroup(uid, count, posting :: postings, sender :: senders)
+class PostingGroup(count: Int, var postings: List[Posting], var senders: List[ActorRef]) extends Actor {
+  def add(count: Int, posting: Posting, sender: ActorRef): Unit = {
+    if (count != this.count)
+      throw new LedgerException("count mismatch")
+    else {
+      postings = posting :: postings
+      senders = sender :: senders
+    }
   }
 
   def ready: Boolean = postings.size == count
@@ -46,30 +53,48 @@ class PostingGroup(uid: String, count: Int, postings: List[Posting], senders: Li
       if (!journal.audit)
         throw new LedgerException("Journal fails audit")
       journal
+    }
+    else
+      throw new LedgerException("Postings not ready")
+  }
+
+  def receive = {
+    case AddPosting(c, p, s) =>
+      add(c, p, s)
+      if (ready)
+        context.parent ! NewJournal(toJournal(""))
+        senders.foreach(_ ! PostingResult(result = true))
   }
 }
 
 case class Journal(typ: String, postings: List[Posting]) {
   val timestamp = DateTime.now
 
-  def audit: Boolean = postings.foldLeft(0: Quantity)((x, y) => x + y.sign * y.quantity) == 0
-
+  def audit: Boolean = postings.groupBy(_.contract).forall(_._2.map(_.signedQuantity).sum == 0)
 }
 
 class Ledger extends Actor {
-  var ledger = List[Journal]
-  val pending = mutable.Map[String, PostingGroup]()
+  var ledger = List[Journal]()
+  val pending = mutable.Map[String, ActorRef]()
 
   def getBalance(user: User, contract: Contract, timestamp: DateTime = DateTime.now): Quantity = {
-    for {
-      posting@Posting(contract, user, quantity, _) <- ledger
+    val qtys = for {
+        entries<- ledger
+        posting@Posting(contract, user, quantity, _) <- entries
       if posting.timestamp <= timestamp
     } yield posting.signedQuantity
+    qtys.sum
   }
 
   def receive = {
-    case NewPosting(posting, count) =>
+    case NewPosting(count, posting, uuid) =>
+      if (pending contains uuid)
+        pending(uuid) ! AddPosting(count, posting, sender())
+      else
+        pending(uuid) = context.actorOf(Props(new PostingGroup(count, List(posting), List(sender()))), name = uuid)
     case GetBalance(user, contract) => sender ! getBalance(user, contract)
+    case NewJournal(journal) =>
+      ledger = journal :: ledger
   }
 }
 
