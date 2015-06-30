a517e1b0534dd6b82095c84d780e5b36f16a3283
Sameer Parekh Brenn <sameer@creativedestruction.com>
2015-06-30 17:05:52 -0600

    ensure the accountant recvs the messages it should add cancel test

--- test/EngineSpec.scala
+++ test/EngineSpec.scala
@@ -4,7 +4,7 @@
 
 import akka.actor.ActorSystem
 import models._
-import actors.{OrderBookClassifier, SputnikEventBus, Engine}
+import actors.{Accountant, OrderBookClassifier, SputnikEventBus, Engine}
 import org.scalatest.{WordSpecLike, WordSpec}
 import akka.testkit._
 
@@ -48,6 +48,9 @@ class EngineSpec extends TestKit(ActorSystem("testSystem")) with WordSpecLike wi
         }
         expectMsg(book)
       }
+      "send no msgs to accountant" in {
+        accountantProbe.expectNoMsg()
+      }
     }
     "given two orders that match" should {
       val engine = TestFSMRef(new Engine(btcusd))
@@ -70,6 +73,38 @@ class EngineSpec extends TestKit(ActorSystem("testSystem")) with WordSpecLike wi
         }
         assert(msgs(1) == book)
       }
+      "send the trade to accountant" in {
+        val msg = accountantProbe.receiveN(1)(0)
+        msg match {
+          case Accountant.TradeNotify(trade: Trade, _) =>
+            assert(trade.quantity == 100L)
+            assert(trade.price == 100L)
+        }
+      }
     }
+    "given an order and a cancel" should {
+      val engine = TestFSMRef(new Engine(btcusd))
+      val accountantProbe = TestProbe()
+      engine ! Engine.SetAccountantRouter(accountantProbe.ref)
+      SputnikEventBus.subscribe(testActor, OrderBookClassifier(Some(btcusd)))
+      engine ! Engine.PlaceOrder(buy100At100)
+      engine ! Engine.CancelOrder(btcusd, buy100At100._id)
+      "have an empty book" in {
+        val book = engine.stateData match {
+          case Engine.Initialized(orderBook: OrderBook, _) => orderBook
+        }
+        assert(book.bids.isEmpty)
+        assert(book.asks.isEmpty)
+      }
+      "publish two orderbooks" in {
+        val msgs = receiveN(2)
+        val book = engine.stateData match {
+          case Engine.Initialized(orderBook: OrderBook, _) => orderBook
+        }
+        assert(msgs.head.asInstanceOf[OrderBook].bids contains buy100At100)
+        assert(msgs(1) == book)
+      }
+    }
+
   }
 }
