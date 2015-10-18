617324d4d3e2738edae74c44f17cb1676a69eb91
Yury Sobolev <yury.sobolev@gmail.com>
2015-10-18 03:11:20 -0400

    Switch over to setuptools.

--- server/setup.py
+++ server/setup.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from pkgutil import walk_packages
-
-def find_packages():
-    for _, name, ispkg in walk_packages("."):
-        if ispkg:
-            yield name
+from setuptools import setup, find_packages
 
 setup(name="Sputnik",
       version="1.3.0",
       description="Sputnik Exchange",
       author="Mimetic Markets",
       url="https://m2.io",
-      packages=list(find_packages()),
+      packages=find_packages(),
       package_data={"sputnik.templates": ["*.html", "*.email"],
                     "sputnik.specs": ["objects/*.json", "rpc/*.json",
                                       "public/*.json"]}
