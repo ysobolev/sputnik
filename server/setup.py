bc0c53c2063da56772aad2be145a6cd7f9816dc2
Yury Sobolev <yury.sobolev@gmail.com>
2015-12-17 01:01:37 -0500

    Add install_requires.

--- server/setup.py
+++ server/setup.py
@@ -10,6 +10,8 @@ setup(name="Sputnik",
       packages=find_packages(),
       package_data={"sputnik.templates": ["*.html", "*.email"],
                     "sputnik.specs": ["objects/*.json", "rpc/*.json",
-                                      "public/*.json"]}
+                                      "public/*.json"]},
+      install_requires=["Twisted", "txZMQ==0.7.3", "jsonschema>=2.3.0",
+                        "autobahn==0.9.5"]
      )
       
